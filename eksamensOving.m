5e1f7380f6b2b652da1f1b3fb116c01fdb2f33d7
Øystein Solbø <47692941+oyssolbo@users.noreply.github.com>
2020-11-28 07:45:37 +0100

    Update eksamensOving.m
    Mer oversiktlig. Fylte inn LQR med try/catch-blokk

--- eksamensOving.m
+++ eksamensOving.m
@@ -1,59 +1,109 @@
-A = [ 0 1; 0 1];
-B = [0; 1];
+%% Matrices
+A = [1 0; 
+     0 1];
+ 
+B = [0; 
+     1];
+
 C = [1 0];
 
-%% observabillity
+D = 0;
+
+% Disturbances
+G = [0;
+     0];
+ 
+% Noise
+H = 0;
+
+%% Observabillity
 o = obsv(A, C); 
 r_o = rank(o); 
 
-%% controlabillity
+%% Controlabillity
 c = ctrb(A,B); 
-r_c= rank(c);
+r_c = rank(c);
 
 %% Eigenvalues and vectors
-[v, lambda ]= eig(A);
+% Important: Matlab normalizes all eqigenvectors by default. Divide by the
+% lowest eigenvector to acchieve unity
+[v, lambda] = eig(A);
 dim = size(lambda);
-lambda = lambda * ones([dim(1),1]); % convert from a diagnoal matrix to a vector
-%note that all eigenvectors are normilazed
+lambda = lambda * ones([dim(1),1]); % Convert from a diagonal matrix to a vector
 
-%% transferfunction
-A = [0 -4; 1 0]; B = [-2; 0]; C = [0 1]; D = 1;
-sys = ss(A,B,C,D); % state space model
-g = tf(sys);
+%% Transfer function
+sysc = ss(A,B,C,D); % Continous state space model
+g = tf(sysc);
 
-%% inverse laplace
+%% Inverse laplace
 syms s;
 re = ilaplace((s^2 +2)/(s^2 + 4));
 
-%% lyaponov
-% N = eye(2);
-% M = lyap(A,N);  %requires unike solution
+%% Lyaponov
+% Warning: Matlab requires unique solution
+N = eye(2);
+M = lyap(A,N);  
 
-%% positive definite check
-M = [1 0; 0 1];
+%% Positive definite check
 try chol(M)
-    disp("Positive definite")
+    disp(" ");
+    disp("Lyapunov positive definite")
 catch ME
-    disp("Not positve definite")
+    disp(" ");
+    disp("Lyapunov not positive definite")
 end
-%% LQR
 
-%% discrete KF
+%% LQR
+Q = eye(1);
+R = eye(1);
+try 
+    K_lqr = lqr(A, B, Q, R);
+catch ME
+    disp(" ");
+    disp("Not possible to stabilize system using LQR");
+    K_lqr = 0;
+end
+%% Discrete KF
 n = 3; %number of iterations - 1
-A_d = 1; B_d = 1; C_d = 1;
-u = zeros(1,n); y = [1 2 1];
-Q_d = 2; R_d = 3;
-K=zeros(1,n); x_bar = 0; P_bar = eye(1); %inital values
-for k = 1: n
-    K(k) = (P_bar(k)*C_d')*(C_d*P_bar(k)*C_d + R_d)^(-1);
-    x_hat(k) = x_bar(k) + K(k)*(y(k) - C_d*x_bar(k));
-    P_hat(k) = (eye(1) -K(k)*C_d)*P_bar(k)*(eye(1)-K(k)*C_d)' + K(k)*R_d*K(k)';
+
+% If discretized from known system:
+T_s = 1;    % Sample time
+sysd = c2d(sysc, T_s);
+A_d = sysd.A;
+B_d = sysd.B;
+C_d = sysd.C;
+D_d = sysd.D;
+
+% If not known system:
+A_d = 1; 
+B_d = 1; 
+C_d = 1;
+D_d = 0;
+
+% Input and measurement
+u = zeros(1,n); 
+y = [1 2 1];
+
+% Variance from the given system
+Q_d = 2; 
+R_d = 3;
+
+% Initial values
+K_kf = zeros(1,n); 
+x_bar = 0; 
+P_bar = eye(1); 
+
+for k = 1:n
+    K_kf(k) = (P_bar(k)*C_d')*(C_d*P_bar(k)*C_d + R_d)^(-1);
+    x_hat(k) = x_bar(k) + K_kf(k)*(y(k) - C_d*x_bar(k));
+    P_hat(k) = (eye(1) -K_kf(k)*C_d)*P_bar(k)*(eye(1)-K_kf(k)*C_d)' + K_kf(k)*R_d*K_kf(k)';
     x_bar(k+1) = A_d*x_hat(k) + B_d*u(k);
     P_bar(k+1) = A_d*P_hat(k)*A_d' + Q_d;
 end
 
-%% tips and tricks
- syms m_1 m_2 m_3;
+%% Other
+syms m_1 m_2 m_3;
  
-M = [m_1 m_2; m_2 m_3];
+M = [m_1 m_2; 
+     m_2 m_3];
 M_merket = M^(-1);
