2eb08a8893da044c18ad55b5d8be0d9cd2426e4b
Øystein Solbø <47692941+oyssolbo@users.noreply.github.com>
2020-11-28 10:26:10 +0100

    Update eksamensOving.m
    Added a hard-coded solution for the ricatti-equation for cont_KF

--- eksamensOving.m
+++ eksamensOving.m
@@ -1,11 +1,11 @@
 %% Matrices
-A = [1 0; 
-     0 1];
+A = [-1 1; 
+     1  -1];
  
-B = [0; 
-     1];
+B = [1; 
+     0];
 
-C = [1 0];
+C = [1/2 1/2];
 
 D = 0;
 
@@ -17,19 +17,24 @@ G = [0;
 H = 0;
 
 %% Observabillity
-o = obsv(A, C); 
-r_o = rank(o); 
+O_obs = obsv(A, C); 
+rank_o = rank(O_obs); 
 
 %% Controlabillity
-c = ctrb(A,B); 
-r_c = rank(c);
+C_con = ctrb(A,B); 
+rank_c = rank(C_con);
 
 %% Eigenvalues and vectors
 % Important: Matlab normalizes all eigenvectors by default. Divide by the
 % lowest eigenvector to acchieve unity
-[v, lambda] = eig(A);
+[v, lambda] = eig(A)
 dim = size(lambda);
-lambda = lambda * ones([dim(1),1]); % Convert from a diagonal matrix to a vector
+%lambda = lambda * ones([dim(1),1]); % Convert from a diagonal matrix to a vector
+
+% Finding the inverse eigenmatrix - if it exists
+if det(A) ~= 0
+    v_inv = inv(v)
+end
 
 %% Transfer function
 sysc = ss(A,B,C,D); % Continous state space model
@@ -37,15 +42,19 @@ g = tf(sysc);
 
 %% Inverse laplace
 syms s;
-re = ilaplace((s^2 +2)/(s^2 + 4));
+time_system = ilaplace((s^2 +2)/(s^2 + 4));
 
 %% Lyaponov
 % Warning: Matlab requires unique solution
 N = eye(2);
-M = lyap(A,N);  
+try 
+    P = lyap(A,N);  
+catch ME
+    disp("Lyapunov not possible to evaluate")
+end
 
-%% Positive definite check
-try chol(M)
+%% Lyapunov - Positive definite check
+try chol(P)
     disp(" ");
     disp("Lyapunov positive definite")
 catch ME
@@ -65,46 +74,64 @@ catch ME
 end
 
 %% Discrete KF
-n = 3; %number of iterations - 1
+n = 5; % Number of iterations + 1
 
 % If discretized from known system:
-T_s = 1;    % Sample time
-sysd = c2d(sysc, T_s);
+T_s = 1;                % Sample time
+sysd = c2d(sysc, T_s);  % Discrete system model
 A_d = sysd.A;
 B_d = sysd.B;
 C_d = sysd.C;
 D_d = sysd.D;
 
-% If not known system:
+% If system already discretized:
 A_d = 1; 
 B_d = 1; 
 C_d = 1;
 D_d = 0;
 
 % Input and measurement
-u = zeros(1,n); 
-y = [1 2 1];
+u = [100 105 92 105 102]; 
+y = [10 91 220 288 405];
 
 % Variance from the given system
-Q_d = 2; 
-R_d = 3;
+Q_d = 4; 
+R_d = 25;
 
 % Initial values
-K_kf = zeros(1,n); 
-x_bar = 0; 
-P_bar = eye(1); 
+K_kf_disc = zeros(1,n); 
+x_pri = 0; 
+P_pri = 12; 
 
 for k = 1:n
-    K_kf(k) = (P_bar(k)*C_d')*(C_d*P_bar(k)*C_d + R_d)^(-1);
-    x_hat(k) = x_bar(k) + K_kf(k)*(y(k) - C_d*x_bar(k));
-    P_hat(k) = (eye(1) -K_kf(k)*C_d)*P_bar(k)*(eye(1)-K_kf(k)*C_d)' + K_kf(k)*R_d*K_kf(k)';
-    x_bar(k+1) = A_d*x_hat(k) + B_d*u(k);
-    P_bar(k+1) = A_d*P_hat(k)*A_d' + Q_d;
+    K_kf_disc(k) = (P_pri(k)*C_d')*(C_d*P_pri(k)*C_d + R_d)^(-1);
+    x_hat(k) = x_pri(k) + K_kf_disc(k)*(y(k) - C_d*x_pri(k));
+    P_hat(k) = (eye(1) - K_kf_disc(k)*C_d)*P_pri(k)*(eye(1) - K_kf_disc(k)*C_d)' + K_kf_disc(k)*R_d*K_kf_disc(k)';
+    x_pri(k+1) = A_d*x_hat(k) + B_d*u(k);
+    P_pri(k+1) = A_d*P_hat(k)*A_d' + Q_d;
 end
 
-%% Other
-syms m_1 m_2 m_3;
- 
-M = [m_1 m_2; 
-     m_2 m_3];
-M_merket = M^(-1);
+%% Continous KF
+syms p_11 p_12 p_22;
+
+% Variance from the given system
+Q_c = 2; 
+R_c = 3;
+
+P = [p_11 p_12; 
+     p_12 p_22];
+
+% Hardcoded solution of the ricatti-equation. Could be improved by using
+% an in-built function
+P_dot = A*P + P*A' + G*Q_c*G' - P*C'*inv(R_c)*C*P == zeros(2);
+P_sol = solve(P_dot,[p_11 p_12 p_22]);
+p_11 = subs(P_sol.p_11);
+p_12 = subs(P_sol.p_12);
+p_22 = subs(P_sol.p_22);
+
+
+P = [p_11 p_12;
+     p_12 p_22];
+
+% Kalman gain in the continous time
+K_kf_cont = P*C'*inv(R);
